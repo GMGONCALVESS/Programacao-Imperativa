99baf21877ac464176bb3f6c19c611cee3b33686
Gabriel Mariano <gabrielmarianogsantos@gmail.com>
2021-06-10 15:23:30 -0300

    Exercício lista de compras - Gabriel Mariano

---
+++ listaDeCompras.js
@@ -0,0 +1,17 @@
+//https://github.com/GMGONCALVESS/Programacao-Imperativa.git
+//Join, Pop, Push, Shift, Unshift
+
+listaDeCompras = ["Sabonete", "Detergente", "Açúcar", "Leite"];
+item = "Queijo";
+
+console.log("O método join apresenta os dados do array, por padrão eles são separados por vírgula.")
+console.log(listaDeCompras.join() + "\n");
+console.log("O método pop ele retira o último elemento do array, e retorna o elemento para o usuário.");
+console.log(listaDeCompras.pop() + "\n");
+console.log("O método push adicona um elemento no final do array, e nos retorna a sua posição.");
+console.log(listaDeCompras.push(item) + "\n");
+console.log("O método shift retira o primeiro elemento do array, e retorna o elemento para o usuário.");
+console.log(listaDeCompras.shift() + "\n");
+console.log("O método unshift adiciona um elemento no começo do array, e retorna a sua posição.");
+console.log(listaDeCompras.unshift(item));
+
